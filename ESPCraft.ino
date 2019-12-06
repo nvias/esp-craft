e8ba3732f0c919c6502fe50e26ea333f44fa6a9f
jblaha <jakub.blaha@incolor.cz>
2019-12-06 23:18:28 +0100

    added config, parsing data update

--- ESPCraft.ino
+++ ESPCraft.ino
@@ -1,23 +1,20 @@
 #include <ESP8266WiFi.h>
 #include <Adafruit_NeoPixel.h>
+#include <stdlib.h>
 #include "PubSubClient.h"
+#include "config.h"
 
-/********************** ESP CRAFT SETUP **************************/
-#define MODULE_NUMBER "4"
-/********************* END ESP CRAFT SETUP **********************/
+#define BTN1 D1
+#define BTN2 D2
 
-#define MQTT_PREFIX "nvias/MC"
+#define LED_PIN D3
+#define LED_COUNT 2
 
-#define SUB_TOPIC MQTT_PREFIX "/M" MODULE_NUMBER "/#"
-#define PUB_TOPIC MQTT_PREFIX "/M" MODULE_NUMBER
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
 
-#define BTN 14
-
-const char* ssid = "JMENO_WIFI";
-const char* password = "HELSO_WIFI";
-
-const char* mqtt_server = "52.59.197.227";
-const uint16_t mqtt_port = 1884;
+const char* mqtt_server = MQTT_SERVER;
+const uint16_t mqtt_port = MQTT_PORT;
 
 char* subTopic = SUB_TOPIC;
 char* pubTopic = PUB_TOPIC;
@@ -25,22 +22,22 @@ char* pubTopic = PUB_TOPIC;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, 0, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void handleMessage(byte* payload){
-  char* rawColors = strrchr((char*)payload, '(');
+    char* rawColors = strrchr((char*)payload, '(');
+    String sPayload = (char *)payload;
 
-  String colors[3] = {"", "", ""};
-  
-  if((char)payload[0] == '1'){
-    for(int i = 1, x = 0; i < strlen(rawColors); i++){   
+    String colors[3] = {"", "", ""};
+
+    for(int i = 1, x = 0; i < strlen(rawColors); i++){
       if(rawColors[i] == ',') {
         x++;
         continue;
       }
-  
+
       if(rawColors[i] == ')') break;
-      
+
       if(x == 0){
         colors[x] += (String)rawColors[i];
       } else if(x == 1){
@@ -50,14 +47,77 @@ void handleMessage(byte* payload){
       }
     }
 
-    Serial.print(colors[0] + "," + colors[1] + "," + colors[2]);
-    
-    pixels.setPixelColor(0, pixels.Color(colors[0].toInt(), colors[1].toInt(), colors[2].toInt()));
-    pixels.show();
-  }else{
-    Serial.print("0");
-    pixels.setPixelColor(0, pixels.Color(0,0,0));
-    pixels.show();
+    char c = sPayload.charAt(0);
+    int pin = (int)c - 48;
+
+    Serial.print(pin);
+    Serial.println(": " + colors[0] + "," + colors[1] + "," + colors[2]);
+
+    if (pin >= 0 && pin <= 1){
+        pixels.setPixelColor(pin, pixels.Color(colors[0].toInt(), colors[1].toInt(), colors[2].toInt()));
+        pixels.show();
+    }
+}
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);
+  pinMode(BTN1, INPUT_PULLUP);
+  pinMode(BTN2, INPUT_PULLUP);
+
+  delay(50);
+  digitalWrite(BUILTIN_LED, 1);
+
+  Serial.begin(115200);
+  pixels.begin();
+
+  startup();
+
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+bool state1 = false;
+bool state2 = false;
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+
+    digitalWrite(BUILTIN_LED, 0);
+  }
+  
+  client.loop();
+
+//////////////////////////////////////// BTN HANDLING
+
+  if(!digitalRead(BTN1)){
+    delay(20);
+    if(!digitalRead(BTN1)){
+      client.publish(BTN_1_PUB_TOPIC, state1 ? "1" : "0");
+
+      state1 = !state1;
+
+      digitalWrite(BUILTIN_LED, 1);
+      delay(100);
+      digitalWrite(BUILTIN_LED, 0);
+
+      while(!digitalRead(BTN1));
+    }
+  }
+
+  if(!digitalRead(BTN2)){
+    delay(20);
+    if(!digitalRead(BTN2)){
+      client.publish(BTN_2_PUB_TOPIC, state2 ? "1" : "0");
+
+      state2 = !state2;
+
+      digitalWrite(BUILTIN_LED, 1);
+      delay(100);
+      digitalWrite(BUILTIN_LED, 0);
+
+      while(!digitalRead(BTN2));
+    }
   }
 }
 
@@ -66,12 +126,10 @@ void callback(char* topic, byte* payload, unsigned int len) {
   Serial.print(topic);
   Serial.print("] ");
 
-  const char* pin = strrchr(topic, '/');
-  
-  if(strcmp(pin, "/D3") == 0){
+  if(strcmp(topic, LED_SUB_TOPIC) == 0){
     handleMessage(payload);
   }
-  
+
   Serial.println();
 }
 
@@ -82,8 +140,14 @@ void reconnect() {
     clientId += String(random(0xffff), HEX);
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
-      client.publish(pubTopic, "hello world");
-      
+
+      String helloMsg = "[M" + (String)MODULE_NUMBER + "] Logged in.";
+
+      char msg[200];
+      helloMsg.toCharArray(msg, helloMsg.length());
+
+      client.publish(pubTopic, msg);
+
       Serial.print("Subscribing to ");
       Serial.println(subTopic);
       client.subscribe(subTopic);
@@ -99,7 +163,7 @@ void reconnect() {
 void setup_wifi() {
   delay(10);
   Serial.println();
-  Serial.print("Connecting to ");
+  Serial.print("Connecting to: ");
   Serial.println(ssid);
 
   WiFi.begin(ssid, password);
@@ -117,43 +181,20 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);
-  pinMode(BTN, INPUT_PULLUP);
-
-  delay(50);
-  digitalWrite(BUILTIN_LED, 1);
-
-  Serial.begin(115200);
-  setup_wifi();
-  client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback);  
-
-  pixels.begin();
-
-  pixels.setPixelColor(0, pixels.Color(0,0,0));
+void startup(){
+  pixels.setPixelColor(0, 0, 0, 200);
   pixels.show();
-}
+  delay(500);
 
-bool state = false;
-void loop() {
-  if (!client.connected()) {
-    reconnect();
+  pixels.setPixelColor(0, 0, 0, 0);
+  pixels.show();
+  delay(500);
 
-    digitalWrite(BUILTIN_LED, 0);
-  }
-  
-  client.loop();
+  pixels.setPixelColor(1, 0, 0, 200);
+  pixels.show();
+  delay(500);
 
-  if(!digitalRead(BTN)){
-    delay(20);
-    if(!digitalRead(BTN)){
-      client.publish("nvias/MC/M4/D5", state ? "1" : "0");
-      state = !state;
-      digitalWrite(BUILTIN_LED, 1);
-      delay(100);
-      digitalWrite(BUILTIN_LED, 0);
-      while(!digitalRead(BTN));
-    }
-  }
+  pixels.setPixelColor(1, 0, 0, 0);
+  pixels.show();
+  delay(500);
 }
--- README.md
+++ README.md
@@ -3,8 +3,12 @@ Arduino CLI
 
 Compile
 -------
- - `arduino-cli compile -b esp8266:esp8266:nodemcuv2`
+ - NodeMCU `arduino-cli compile -b esp8266:esp8266:nodemcuv2`
+ - WeMOS D1 `arduino-cli compile -b esp8266:esp8266:d1`
+ - WeMOS D1 mini `arduino-cli compile -b esp8266:esp8266:d1_mini`
  
 Upload
 ------
- - `arduino-cli upload -p /dev/ttyUSB0 -b esp8266:esp8266:nodemcuv2:baud=921600`
+ - NodeMCU `arduino-cli upload -p /dev/ttyUSB0 -b esp8266:esp8266:nodemcuv2:baud=921600`
+ - WeMOS D1 `arduino-cli upload -p /dev/ttyUSB0 -b esp8266:esp8266:d1:baud=921600`
+ - WeMOS D1 mini `arduino-cli upload -p /dev/ttyUSB0 -b esp8266:esp8266:d1_mini:baud=921600`
